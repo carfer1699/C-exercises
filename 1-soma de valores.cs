e6d4bee1b2fc68b08ba2a346b7b66ec00e22f252
Carlos Ferreira <31482695+carfer1699@users.noreply.github.com>
2019-06-16 13:55:54 -0300

    Add files via upload

---
+++ 1-soma de valores.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace area_da_circunferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, Pi, a;
+            Pi = 3.14159;
+            Console.WriteLine("Informe um valor para o raio da circunferência: ");
+            r = double.Parse(Console.ReadLine());
+
+            a = (Pi * (r * r));
+
+            Console.WriteLine("A área da circunferência é igual a " + a.ToString("F2"));
+
+            Console.ReadKey();
+        }
+    }
+}
